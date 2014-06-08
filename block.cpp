9237262d6cc3af6630b360513f17abe75e2eb950
Ryan Barry <ryan@nuclearice.com>
2014-06-07 18:14:50 -0700

    (been so long, know idea why changes made)

--- block.cpp
+++ block.cpp
@@ -169,3 +169,62 @@ void Block::translate(float x, float y, float z) {
 	if(y != 0.0f) renderable->translateY(y);
 	if(z != 0.0f) renderable->translateZ(z);
 }
+
+std::vector<glm::vec3>* Block::generateVertexCoordinates(std::vector<glm::vec3>* points, float x, float y, float z) {
+	int start = points->size();
+	
+	// front face
+	(*points)[start+0]  = glm::vec3(-0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+1]  = glm::vec3(0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+2]  = glm::vec3(-0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+3]  = glm::vec3(-0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+4]  = glm::vec3(0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+5]  = glm::vec3(0.5f+x, 0.5f+y, 0.5f+z);
+	
+	// right face
+	(*points)[start+6]  = glm::vec3(0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+7]  = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+8]  = glm::vec3(0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+9]  = glm::vec3(0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+10] = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+11] = glm::vec3(0.5f+x, 0.5f+y, -0.5f+z);
+	
+	// rear face
+	(*points)[start+12] = glm::vec3(-0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+13] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+14] = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+15] = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+16] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+17] = glm::vec3(0.5f+x, 0.5f+y, -0.5f+z);
+	
+	// top face
+	(*points)[start+18] = glm::vec3(0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+19] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+20] = glm::vec3(0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+21] = glm::vec3(0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+22] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+23] = glm::vec3(-0.5f+x, 0.5f+y, 0.5f+z);
+	
+	// left face
+	(*points)[start+24] = glm::vec3(-0.5f+x, 0.5f+y, 0.5f+z);
+	(*points)[start+25] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+26] = glm::vec3(-0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+27] = glm::vec3(-0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+28] = glm::vec3(-0.5f+x, 0.5f+y, -0.5f+z);
+	(*points)[start+29] = glm::vec3(-0.5f+x, -0.5f+y, -0.5f+z);
+	
+	// bottom face
+	(*points)[start+
+		30] = glm::vec3(-0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+
+		31] = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+
+		32] = glm::vec3(-0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+
+		33] = glm::vec3(-0.5f+x, -0.5f+y, 0.5f+z);
+	(*points)[start+
+		34] = glm::vec3(0.5f+x, -0.5f+y, -0.5f+z);
+	(*points)[start+
+		35] = glm::vec3(0.5f+x, -0.5f+y, 0.5f+z);
+	
+}
--- block.hpp
+++ block.hpp
@@ -3,6 +3,9 @@
 #include "renderable.hpp"
 #include "shaderprogram.hpp"
 
+#include <vector>
+#include <glm/glm.hpp>
+
 class Block {
 public:
 	Block();
@@ -12,6 +15,7 @@ public:
 	void translate(float x, float y, float z);
 	
 private:
+  std::vector<glm::vec3>* generateVertexCoordinates(std::vector<glm::vec3>* points, float x, float y, float z);
 	static const float vertexCoordinates[];
 	static const float vertexNormals[];
 	static const float vertexTextureCoordinates[];
